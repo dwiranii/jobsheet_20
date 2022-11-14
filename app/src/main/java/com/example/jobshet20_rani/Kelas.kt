875720686fd7c165eb02d1a4e5d4b8799a8960ff
NorHidayah <salma@gmail.com>
2022-11-14 12:20:23 +0700

    first commit

---
+++ app/src/main/java/com/example/jobshet20_rani/Kelas.kt
@@ -0,0 +1,9 @@
+package com.example.jobshet20_rani
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+data class Kelas(
+    val nama: String?,
+    val nis: String?
+)
---
+++ app/src/main/java/com/example/jobshet20_rani/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.jobshet20_rani
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    val list = ArrayList<Kelas>()
+    val listNama = arrayOf(
+        "ARDIAN MUHAMMAD IQBAL",
+        "BAGAS SAPUTRA",
+        "CHELSIA ADITYA SUYATNO",
+        "DIMAS ILHAMSYAH RAMADHAN",
+        "DINA MUSTIKA",
+        "DWI RANI SYARIFAH",
+        "FIFIT SHOLIKHATUL MUFALAH",
+        "FIKA ISMATUL HAWA",
+        "FIKI ZULKARNAIN",
+        "HEYDARISTO ZHAHIR RAZAQ",
+        "IKHA NUR ROCHAYATIN",
+    )
+
+    val listNis = arrayOf(
+        "2976",
+        "2978",
+        "2980",
+        "2983",
+        "2984",
+        "2986",
+        "2989",
+        "2990",
+        "2991",
+        "2996",
+        "2998",
+    )
+    lateinit var kelasView: RecyclerView
+    lateinit var kelasAdapter: KelasAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        kelasView = findViewById(R.id.kelasView)
+        kelasView.layoutManager = LinearLayoutManager(this)
+
+        for (i in listNama.indices) {
+            list.add(Kelas(listNama[i],listNis[i]))
+        }
+
+        kelasAdapter = KelasAdapter(list)
+        kelasAdapter.notifyDataSetChanged()
+        kelasView.adapter = kelasAdapter
+    }
+
+}
